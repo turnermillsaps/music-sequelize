3062e9a4743791aa8bd6f980808430f61354a322
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-05-08 21:49:59 -0400

    create_artist.js prompts user and successfully inserts a new record into artist table

--- create_artist.js
+++ create_artist.js
@@ -1,11 +1,13 @@
 const prompt = require('prompt-promise');
 const Sequelize = require('sequelize');
 
+// Instantiate new Sequelize class
 const sequelize = new Sequelize('musicseq','postgres','ph1shstix',{
     host: 'localhost',
     dialect: 'postgres'
 })
 
+// Authenticate with the database
 sequelize.authenticate()
     .then(() => {
         console.log('Connection established successfully');
@@ -14,21 +16,40 @@ sequelize.authenticate()
         console.error('Unable to connect to database: ' + err);
     })
 
+// Create model for artist table
 const Artist = sequelize.define('Artist', {
     artist_name: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
     }
 }, {
     sequelize,
     modelName: 'artist',
     tableName: 'artist',
-    freezeTableName: true
+    freezeTableName: true,
+    timestamps: false
 })
 
+// Create table if it does not exist, otherwise update
 Artist.sync({ logging: console.log })
     .then(() => {
         console.log('Table synced successfully');
+        prompt('Artist name? ') // Prompt user for a new artist once connection is established
+            .then((val) => {
+                sequelize.query(`INSERT INTO artist (artist_name) VALUES ('${val}') RETURNING id`, { raw: true })
+                    .then((res) => {
+                        console.log(`Record created with id: ${res[0][0].id}`);
+                        sequelize.close();
+                    })
+                    .catch((err) => {
+                        console.error(`Error inserting record into artist table: ${err}`);
+                        sequelize.close();
+                    })
+            })
     })
     .catch((err) => {
         console.error('Unable to sync with table: ' + err);
